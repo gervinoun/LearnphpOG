68750b554435c24e06540db69550414c93e0dacb
gervinoun <gervinpro@gmail.com>
2026-09-24 11:31:01 +0300

    dockercomposei lisamine andmebaasi

--- routes.php
+++ routes.php
@@ -21,3 +21,25 @@ Router::addRoute('/us', function () {
 
     include __DIR__ . '/views/us.php';
 });
+
+Router::addRoute('/test', function () {
+    $servername = "localhost";
+    $username = "root";
+    $password = "example";
+    $dbname = "learnphp";
+
+    try {
+        $conn = new PDO(
+            "mysql:host=$servername;dbname=$dbname",
+            $username,
+            $password
+        );
+
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "Connected successfully";
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+});
--- src/DB.php
+++ src/DB.php
@@ -4,4 +4,26 @@ namespace App;
 
 class DB
 {
+    public function __construct()
+    {
+        $servername = "localhost:33061";
+        $username = "root";
+        $password = "example";
+        $dbname = "learnphp";
+
+        try {
+            $conn = new PDO(
+                "mysql:host=$servername;dbname=$dbname",
+                $username,
+                $password
+            );
+
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            echo "Connected successfully";
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
 }

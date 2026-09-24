7346b3fc27214530f056cd84a2b71d0bfc895eff
gervinoun <gervinpro@gmail.com>
2026-09-24 09:26:49 +0300

    Add Autoload and namespace examples

--- public/index.php
+++ public/index.php
@@ -4,25 +4,44 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
     return false;
 }
 
-function dump(...$values) {
+function dump(...$values)
+{
     echo '<pre>';
     var_dump(...$values);
     echo '</pre>';
 }
 
-switch ($_SERVER['REQUEST_URI']) {
-    case '/':
-        include __DIR__ . '/../views/index.php';
-        break;
+spl_autoload_register(function ($class) {
+    $class = str_replace('App\\', '', $class);
+    $class = str_replace('\\', '/', $class);
+    require_once __DIR__ . "/../src/$class.php";
+});
 
-    case '/us':
-        include __DIR__ . '/../views/us.php';
-        break;
 
-    case '/technology':
-        include __DIR__ . '/../views/technology.php';
-        break;
+use App\Controllers\PublicController as PC;
+$router = new App\Router();
+$db = new App\DB();
+$controller = new PC();
+$controller = new PC();
+$controller = new PC();
+$controller = new PC();
+$controller = new PC();
+$controller = new App\Controllers\PublicController();
 
-    default:
-        echo '404';
-}
+dump($router, $db, $controller);
+// switch ($_SERVER['REQUEST_URI']) {
+//     case '/':
+//         include __DIR__ . '/../views/index.php';
+//         break;
+
+//     case '/us':
+//         include __DIR__ . '/../views/us.php';
+//         break;
+
+//     case '/technology':
+//         include __DIR__ . '/../views/technology.php';
+//         break;
+
+//     default:
+//         echo '404';
+// }>
